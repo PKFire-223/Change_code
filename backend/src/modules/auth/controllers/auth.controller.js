bd79155d49bc184dcbcf583eef31379d20cc75c6
PKFire-223 <nhatquang2703@gmail.com>
2026-04-24 01:57:16 +0700

    update

--- backend/src/modules/auth/controllers/auth.controller.js
+++ backend/src/modules/auth/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const authService = require('../services/auth.service');
+const passwordService = require('../services/password.service');
 
 exports.login = async (req, res, next) => {
     try {
@@ -6,5 +7,24 @@ exports.login = async (req, res, next) => {
         res.status(200).json({ success: true, data });
     } catch (error) {
         next(error);
+    }
+};
+
+exports.forgotPassword = async (req, res, next) => {
+    try {
+        await passwordService.forgotPassword(req.body.email);
+        res.status(200).json({ success: true, message: 'Password reset link sent to email' });
+    } catch (error) { 
+        next(error); 
+    }
+};
+
+exports.resetPassword = async (req, res, next) => {
+    try {
+        const { email, token, newPassword } = req.body;
+        await passwordService.resetPassword(email, token, newPassword);
+        res.status(200).json({ success: true, message: 'Password has been reset successfully' });
+    } catch (error) { 
+        next(error); 
     }
 };
